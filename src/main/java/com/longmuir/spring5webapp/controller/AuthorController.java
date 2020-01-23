692f51cd2cdca6ecf3e856cd019c5a57fa052f56
Johanna Longmuir <johannalongmuir@gmail.com>
2020-01-23 11:38:50 +0000

    Refactor autowired annotation instead of constructor

--- src/main/java/com/longmuir/spring5webapp/controller/AuthorController.java
+++ src/main/java/com/longmuir/spring5webapp/controller/AuthorController.java
@@ -1,6 +1,7 @@
 package com.longmuir.spring5webapp.controller;
 
 import com.longmuir.spring5webapp.repositories.AuthorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,12 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class AuthorController {
 
+    @Autowired
     private AuthorRepository authorRepository;
-
-    public AuthorController(AuthorRepository authorRepository) {
-        this.authorRepository = authorRepository;
-    }
-
 
     @RequestMapping("/authors")
     public String getAuthors(Model model){
