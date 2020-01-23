84e720ac878f56fa26ad096b3fd72e830f57723f
Johanna Longmuir <johannalongmuir@gmail.com>
2020-01-23 13:28:22 +0000

    Empty Test class for Book Controller

---
+++ src/test/java/com/longmuir/spring5webapp/controller/BookControllerTest.java
@@ -0,0 +1,14 @@
+package com.longmuir.spring5webapp.controller;
+
+import jdk.nashorn.internal.ir.annotations.Ignore;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Ignore
+class BookControllerTest {
+
+    @Test
+    void getBooks() {
+    }
+}
